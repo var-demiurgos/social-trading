2b0b041073a1bf31308528e8576776b6a83bf821
tomopooh <stomo834@gmail.com>
2019-08-27 13:34:28 +0900

    make it better

--- trade/serializer.py
+++ trade/serializer.py
@@ -5,24 +5,24 @@ from django.utils import timezone
 
 class AccountSerializer(serializers.ModelSerializer):
   class Meta:
-    model = Account
-    fields = ('account_id', 'account_pass', 'account_num', 'active', 'post_time', 'last_login', 'comment')
+	model = Account
+	fields = ('account_id', 'account_pass', 'account_num', 'active', 'post_time', 'last_login', 'comment')
 
 class AccountFilter(filters.FilterSet):
-    account_num = filters.NumberFilter(lookup_expr='exact')
-    active = filters.CharFilter(lookup_expr='exact')
+	account_num = filters.NumberFilter(lookup_expr='exact')
+	active = filters.CharFilter(lookup_expr='exact')
 
-    class Meta:
-        model = Account
-        fields = ('account_num', 'active')
+	class Meta:
+		model = Account
+		fields = ('account_num', 'active')
 
-    def update(self, validated_data):
-    	print("AC:"+validated_data['account_num'])
-        account = Account.objects.get(account_num=validated_data['account_num']).updata(last_login=timezone.now)
-        account.save()
-        return user
+	def update(self, validated_data):
+		print("AC:"+validated_data['account_num'])
+		account = Account.objects.get(account_num=validated_data['account_num']).updata(last_login=timezone.now)
+		account.save()
+		return account
 
 class Trade_ListSerializer(serializers.ModelSerializer):
   class Meta:
-    model = Trade_List
-    fields = ('ticket','order_type','lot','stoploss','takeprofit','open_price')
+	model = Trade_List
+	fields = ('ticket','order_type','lot','stoploss','takeprofit','open_price')
