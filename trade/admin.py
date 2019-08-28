407c866c6b78e95b985be0de3b302eb3454360c1
tomopooh <stomo834@gmail.com>
2019-08-28 13:44:01 +0900

    make it better

--- trade/admin.py
+++ trade/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Account, Trade_List
+from .models import Account, Trade_List, Test
 
 # Register your models here.
 
 admin.site.register(Account)
 admin.site.register(Trade_List)
+admin.site.register(Test)
